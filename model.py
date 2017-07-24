809b37d241046c10b0f86450aa67796479db4563
Sarvagya Pant <sarvagya@Sarvagyas-MacBook-Pro.local>
2017-07-24 11:44:25 -0500

    Model updated

--- model.py
+++ model.py
@@ -2,12 +2,19 @@ import csv
 import cv2
 import numpy as np
 
+
+from keras.models import Sequential
+from keras.layers import Flatten, Dense, Lambda
+from keras.layers.convolutional import Convolution2D
+from keras.layers.pooling import MaxPooling2D
+
+
 class DataReader:
     """This class reads the file and returns required data"""
-
     def __init__(self, filename):
         self.filename = filename
         self.read_file()
+
 
     def read_file(self):
         imgs = []
@@ -35,27 +42,46 @@ class DataReader:
     def get_measurements(self):
         return self.measurements
 
+class KerasModel(object):
+    def __init__(self, x_train, y_train):
+        self.model = None
+        self.x_train = x_train
+        self.y_train = y_train
+
+    def make_conv_layers(self):
+        self.model = Sequential()
+        self.model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3)))
+        # Add Conv Layers based on the paper https://arxiv.org/pdf/1604.07316v1.pdf
+        self.model.add(Conv2D(filters=24, kernel_size=(5,5), strides=(2,2), activation="relu"))
+        self.model.add(MaxPooling2D())
+        self.model.add(Conv2D(filters=36, kernel_size=(5,5), strides=(2,2), activation="relu"))
+        self.model.add(MaxPooling2D())
+        self.model.add(Conv2D(filters=48, kernel_size=(5,5), strides=(2,2), activation="relu"))
+        self.model.add(MaxPooling2D())
+        self.model.add(Conv2D(filters=64, kernel_size=(3,3), activation="relu"))
+        self.model.add(MaxPooling2D())
+        self.model.add(Conv2D(filters=64, kernel_size=(3,3), activation="relu"))
+        self.model.add(MaxPooling2D())
+
+        self.model.add(Flatten())
+        self.model.add(Dense(100))
+        self.model.add(Dense(50))
+        self.model.add(Dense(10))
+        self.model.add(Dense(1))
+        self.model.compile(loss='mse', optimizer='adam')
+
+    def train_model(self):
+        self.model.fit(self.x_train, self.y_train, validation_split = 0.2, shuffle = True, nb_epoch = 5)
+
+    def save_model(self):
+        self.model.save("model.h5")
 
 data = DataReader("data/driving_log.csv")
 x_train = data.get_imgs()
 y_train = data.get_measurements()
 
-from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda
-from keras.layers.convolutional import Convolution2D
-from keras.layers.pooling import MaxPooling2D
-
-model = Sequential()
-model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3)))
-model.add(Convolution2D(6,5,5, activation="relu"))
-model.add(MaxPooling2D())
-model.add(Convolution2D(6,5,5, activation="relu"))
-model.add(MaxPooling2D())
-model.add(Flatten())
-model.add(Dense(120))
-model.add(Dense(84))
-model.add(Dense(1))
-model.compile(loss='mse', optimizer='adam')
-model.fit(x_train, y_train, validation_split = 0.2, shuffle = True, nb_epoch = 5)
-model.save('model.h5')
+km = KerasModel(x_train, y_train)
+km.make_conv_layers()
+km.train_model()
+km.save_model()
                                                                                                       
