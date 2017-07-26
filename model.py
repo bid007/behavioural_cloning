b488c9ddae390666f35069bf099f76c75ecf7ed1
Sarvagya Pant <sarvagya@Sarvagyas-MacBook-Pro.local>
2017-07-26 16:00:47 -0500

    some changes

--- model.py
+++ model.py
@@ -10,10 +10,6 @@ from keras.layers.convolutional import Convolution2D as Conv2D
 from keras.layers.pooling import MaxPooling2D
 from keras.regularizers import l2
 
-
-from keras import backend as K
-print K.image_dim_ordering()
-
 class DataReader:
     """This class reads the file and returns required data"""
     def __init__(self, filename):
@@ -29,16 +25,17 @@ class DataReader:
             reader = csv.reader(f)
             next(reader)
             for row in reader:
-                img_name = row[0].split("/")[-1]
-                current_path = "data/IMG/" + img_name
-                image = cv2.imread(current_path)
-                # Preprocess the image here and append it to the list.
-                image = image[60:140,:] #cv2.resize(image[60:140,:], (64,64))
-                imgs.append(image)
-                imgs.append(cv2.flip(image, 1))
-                steer_angle = float(row[3])
-                angles.append(steer_angle)
-                angles.append(steer_angle * -1)
+                for i in range(3):
+                    img_name = row[i].split("/")[-1]
+                    current_path = "data/IMG/" + img_name
+                    image = cv2.imread(current_path)
+                    # Preprocess the image here and append it to the list.
+                    image = image[60:140,:] #cv2.resize(image[60:140,:], (64,64))
+                    imgs.append(image)
+                    imgs.append(cv2.flip(image, 1))
+                    steer_angle = float(row[3])
+                    angles.append(steer_angle)
+                    angles.append(steer_angle * -1)
 
         self.imgs = np.array(imgs)
         self.measurements = np.array(angles)
@@ -65,7 +62,7 @@ class KerasModel(object):
         # self.model.add(MaxPooling2D())
         self.model.add(Conv2D(48,5,5, activation="relu", border_mode='valid', subsample =(2,2), W_regularizer = l2(0.001)))
         # self.model.add(MaxPooling2D())
-        self.model.add(Conv2D(64,3,3, activation="relu", border_mode='valid', subsample =(2,2), W_regularizer = l2(0.001)))
+        self.model.add(Conv2D(64,3,3, activation="relu", border_mode='same', subsample =(2,2), W_regularizer = l2(0.001)))
         # self.model.add(MaxPooling2D())
         self.model.add(Conv2D(64,3,3, activation="relu", border_mode='valid', subsample =(2,2), W_regularizer = l2(0.001)))
         # self.model.add(MaxPooling2D())
@@ -78,7 +75,7 @@ class KerasModel(object):
         self.model.compile(loss='mse', optimizer='adam')
 
     def train_model(self):
-        self.model.fit(self.x_train, self.y_train, validation_split = 0.2, shuffle = True, nb_epoch = 5)
+        self.model.fit(self.x_train, self.y_train, validation_split = 0.2, shuffle = True, nb_epoch = 1)
 
     def save_model(self):
         self.model.save("model.h5")
