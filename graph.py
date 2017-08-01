c935f623a25d396a4e5b8b79d41248544fe7ba5e
Sarvagya Pant <sarvagya@Sarvagyas-MacBook-Pro.local>
2017-08-01 14:11:41 -0500

    cpu time benchmarks

--- graph.py
+++ graph.py
@@ -1,7 +1,7 @@
 from matplotlib import pyplot as plt
 x = [1,2,4,8,16]
-y_cpu = [166.55,307.40,592.89,None,None]
-y_gpu = []
+y_cpu = [166.55,307.40,592.89,1390.14,2952.36]
+y_gpu = [0,0,0,0,0]
 assert(len(y_gpu) == len(y_cpu))
 l1 = plt.plot(x, y_cpu, "-g",label="CPU Time")
 l2 = plt.plot(x, y_gpu, "-r",label="GPU Time")
